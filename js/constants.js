085a1daa7f21af09c88460dfcf8ba036dc1ae3d0
SafsalGaming <79075952+SafsalGaming@users.noreply.github.com>
2025-12-24 12:53:37 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,10 @@
 export const HEART_URL = "https://cdn.pixabay.com/photo/2017/09/23/16/33/pixel-heart-2779422_1280.png";
 
 export const SKINS = [
-  { id:"yossi_classic", name:"יוסי קלאסי", price:0 },
-  { id:"yossi_bossi",  name:"יוסי בוסי",  price:200 },
-  { id:"yoyo_yossi",   name:"יו יו יוסי",  price:500 },
-  { id:"yossi_dossi",  name:"יוסי דוסי",  price:1000 },
+  { id:"yossi_classic", name:"Classic Yossi", price:0 },
+  { id:"yossi_bossi",  name:"Yossi Bossi",   price:200 },
+  { id:"yoyo_yossi",   name:"Yoyo Yossi",    price:500 },
+  { id:"yossi_dossi",  name:"Yossi Dossi",   price:1000 },
 ];
 
 export const DEFAULT_PROFILE = {
