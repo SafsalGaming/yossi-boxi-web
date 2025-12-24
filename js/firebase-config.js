1c511e5f8cd98b6151f6bf873eee3b3492df882b
SafsalGaming <79075952+SafsalGaming@users.noreply.github.com>
2025-12-24 11:35:02 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,7 @@
 // תדביק כאן את ה-config מה-Firebase Console -> Project settings -> Your apps (Web app)
 window.__FIREBASE_CONFIG__ = {
-  apiKey: "PASTE",
-  authDomain: "PASTE",
-  projectId: "PASTE",
-  appId: "PASTE",
+  apiKey: "AIzaSyCeeG-oMWKE7NrKt4KcD4KIf1781UgnFFQ",
+  authDomain: "yossi-boxi-web.firebaseapp.com",
+  projectId: "yossi-boxi-web",
+  appId: "1:895290035748:web:6525b955f499371a0d92b1",
 };
